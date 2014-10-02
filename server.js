bcc7fe8615d5b9a7959e34b693238262ea7a13a8
Devin Rader <devin@twilio.com>
2014-10-02 18:31:14 -0400

    Maybe azure will like this port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var Hapi = require('hapi')
   , twilio = require('twilio')
   , io;
 
-var server = Hapi.createServer('0.0.0.0', 80);
+var server = Hapi.createServer('0.0.0.0', process.env.PORT || 1337);
 
 
 /**
